78b7ebcdc8b18df7c8c5c1961b91f31a90e0cd2b
davidtrinh173 <davidtrinh173@gmail.com>
2020-02-25 17:32:54 +0700

    commit;

--- Catalog/Implementations/EvalMasterBE.cs
+++ Catalog/Implementations/EvalMasterBE.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EVE.ApiModels.Catalog;
+using EVE.ApiModels.Catalog.Response;
 using EVE.Data;
 using ExtensionMethods;
 
@@ -14,16 +15,19 @@ namespace EVE.Bussiness
         private IEvalCriteriaBE EvalCriteriaBE { get; set; }
         private IEvalPeriodBE EvalPeriodBE { get; set; }
         private IEvalStandardBE EvalStandardBE { get; set; }
+        private IEvalResultBE EvalResultBE { get; set; }
         public EvalMasterBE(IUnitOfWork<EVEEntities> uoW,
                             IEvalDetailBE evalDetailBE,
                             IEvalCriteriaBE evalCriteriaBE,
                             IEvalPeriodBE evalPeriodBE,
-                            IEvalStandardBE evalStandardBE) : base(uoW)
+                            IEvalStandardBE evalStandardBE,
+                            IEvalResultBE evalResultBE) : base(uoW)
         {
             EvalDetailBE = evalDetailBE;
             EvalCriteriaBE = evalCriteriaBE;
             EvalPeriodBE = evalPeriodBE;
             EvalStandardBE = evalStandardBE;
+            EvalResultBE = evalResultBE;
         }
         public async Task<EvalMaster> GetById(EvalMasterBaseReq req)
         {
@@ -74,6 +78,54 @@ namespace EVE.Bussiness
             }
 
             return null;
+        }
+
+        public async Task<EvalResultSumaryRes> GetEvalResultSumary(ExeEvalDetailByMasterIdReq req)
+        {
+            if (req == null)
+                return null;
+            try
+            {
+                var evalMaster = (await GetAsync(p => p.BeEvalEmployeeId == req.BeEvalEmployeeId
+                                                        && p.EvalEmployeeId == req.EvalEmployeeId
+                                                        && p.EvalPeriodId == req.PeriodId))?.FirstOrDefault();
+                if (evalMaster == null)
+                {
+                    return new EvalResultSumaryRes();
+                }
+                else
+                {
+                    var lstEvalResult = EvalResultBE.GetAll();
+                    var lstCaterial = await GetEvalDetailByMasterId(new EvalMasterBaseReq() { EvalMasterId = evalMaster.EvalMasterId });
+                    if (lstCaterial == null || !lstCaterial.Any())
+                    {
+                        return new EvalResultSumaryRes();
+                    }
+                    else
+                    {
+                        var totalCriteria = lstCaterial.Count();
+                        var total = lstCaterial.Sum(p => lstEvalResult.FirstOrDefault(q => q.EvalResultCode == p.EvalResultCode)?.Idx).CheckInt();
+                        var totalCriteriaHasValue = lstCaterial.Where(p => !string.IsNullOrEmpty(p.EvalResultCode)).Count();
+                        var everage = Math.Round((total / totalCriteria).CheckDecimal(), MidpointRounding.AwayFromZero).CheckInt();
+                        var evalResult = lstEvalResult.FirstOrDefault(p => p.Idx == everage);
+                        return new EvalResultSumaryRes()
+                        {
+                            TotalCriteria = totalCriteria,
+                            Total = total,
+                            TotalCriteriaHasValue = totalCriteriaHasValue,
+                            Everage = everage,
+                            EvalResultCode = evalResult?.EvalResultCode,
+                            EvalResultName = evalResult?.EvalResultName
+
+                        };
+                    }
+                }
+            }
+            catch 
+            {
+                return new EvalResultSumaryRes();
+            }
+
         }
 
         public async Task<List<EvalDetail>> ExeEvalDetailByMasterId(ExeEvalDetailByMasterIdReq req)
--- Catalog/Interfaces/IEvalMasterBE.cs
+++ Catalog/Interfaces/IEvalMasterBE.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using EVE.ApiModels.Catalog;
+using EVE.ApiModels.Catalog.Response;
 using EVE.Data;
 
 namespace EVE.Bussiness
 {
     public interface IEvalMasterBE : IBaseBE<EvalMaster>
     {
+        Task<EvalResultSumaryRes> GetEvalResultSumary(ExeEvalDetailByMasterIdReq req);
         Task<EvalMaster> GetByPeriodAndEmployee(EvalMasterGetByPeriodAndEmployeeReq req);
         Task<List<EvalDetail>> GetEvalDetailByMasterId(EvalMasterBaseReq req);
         Task<EvalMaster> GetById(EvalMasterBaseReq req);
