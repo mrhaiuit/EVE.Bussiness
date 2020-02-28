8145bce95a0ce76545fafb6dc2e5edce482f2f6f
mrhaiuit <davidtrinh173@gmail.com>
2020-02-28 22:53:26 +0700

    commit;

--- Catalog/Implementations/FormsBE.cs
+++ Catalog/Implementations/FormsBE.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EVE.ApiModels.Catalog;
@@ -8,8 +9,11 @@ namespace EVE.Bussiness
 {
     public class FormBE : BaseBE<Form>, IFormsBE
     {
-        public FormBE(IUnitOfWork<EVEEntities> uoW) : base(uoW)
+        public IUserGroupFormBE UserGroupFormBE { get; set; }
+        public FormBE(IUnitOfWork<EVEEntities> uoW,
+                    IUserGroupFormBE userGroupFormBE) : base(uoW)
         {
+            UserGroupFormBE = userGroupFormBE;
         }
         public async Task<Form> GetById(GetByUserGroupBaseReq req)
         {
@@ -18,6 +22,23 @@ namespace EVE.Bussiness
                && obj.Any())
             {
                 return obj.FirstOrDefault();
+            }
+
+            return null;
+        }
+
+        public async Task<List<Form>> GetFormsByPermission(UserGroupBaseReq req)
+        {
+            var lstForm = (await UserGroupFormBE.GetAsync(p => p.UserGroupCode == req.UserGroupCode))?.Select(p=>p.FormCode);
+            if(lstForm ==null|| !lstForm.Any())
+            {
+                return null;
+            }
+            var obj = await GetAsync(c => lstForm.Contains(c.FormCode));
+            if (obj != null
+               && obj.Any())
+            {
+                return obj.ToList();
             }
 
             return null;
