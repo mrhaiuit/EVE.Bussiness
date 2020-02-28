8c780521fdbabe25dc8266cf21a979ff1e7b33f8
mrhaiuit <davidtrinh173@gmail.com>
2020-02-28 21:23:32 +0700

    commit;

--- Catalog/Implementations/EvalDetailBE.cs
+++ Catalog/Implementations/EvalDetailBE.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EVE.ApiModels.Catalog;
+using EVE.ApiModels.Catalog.Response;
 using EVE.Data;
 
 namespace EVE.Bussiness
 {
     public class EvalDetailBE : BaseBE<EvalDetail>, IEvalDetailBE
     {
-        public EvalDetailBE(IUnitOfWork<EVEEntities> uoW) : base(uoW)
+        private IEvalResultBE EvalResultBE { get; set; }
+        public EvalDetailBE(IUnitOfWork<EVEEntities> uoW,
+            IEvalResultBE evalResultBE) : base(uoW)
         {
+            EvalResultBE = evalResultBE;
         }
         public async Task<EvalDetail> GetById(EvalDetailBaseReq req)
         {
@@ -18,6 +23,36 @@ namespace EVE.Bussiness
                && obj.Any())
             {
                 return obj.FirstOrDefault();
+            }
+
+            return null;
+        }
+        public async Task<List<EvalDetailByYearAndUserRes>> GetByByYearAndUser(EvalDetailByYearAndUserReq req)
+        {
+            var obj = (this._uoW.Context.EvalDetails.Where(c => c.EvalMaster.EvalPeriod.Year > req.Year - 1
+                                                                && c.EvalCriteriaId == req.EvalCriteriaId
+                                                                && c.EvalMaster.EvalEmployeeId == req.EmployeeId
+                                                                && c.EvalMaster.BeEvalEmployeeId == req.EmployeeId))
+            ?.Select(p => new
+            {
+                p.EvalCriteriaId,
+                p.Sample,
+                p.EvalResultCode,
+                p.EvalMaster.EvalPeriod.Year,
+                p.Attachment
+            }).ToList();
+
+            var lstResult = await EvalResultBE.GetAllAsync();
+            if (obj != null
+               && obj.Any())
+            {
+                return obj.Select(p => new EvalDetailByYearAndUserRes()
+                {
+                    Year = p.Year ?? 0,
+                    EvalResultName = lstResult.FirstOrDefault(t => t.EvalResultCode == p.EvalResultCode).EvalResultName,
+                    Reason = p.Sample,
+                    Attachment = p.Attachment
+                }).ToList();
             }
 
             return null;
--- Catalog/Interfaces/IEvalDetailBE.cs
+++ Catalog/Interfaces/IEvalDetailBE.cs
@@ -1,12 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using EVE.ApiModels.Catalog;
+using EVE.ApiModels.Catalog.Response;
 using EVE.Data;
 
 namespace EVE.Bussiness
 {
     public interface IEvalDetailBE : IBaseBE<EvalDetail>
     {
-
+        Task<List<EvalDetailByYearAndUserRes>> GetByByYearAndUser(EvalDetailByYearAndUserReq req);
         Task<EvalDetail> GetById(EvalDetailBaseReq req);
     }
 }
