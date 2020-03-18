290c92a06c175b8ff4127e8ff8f6d218f3ba5fa7
david <davidtrinh173@gmail.com>
2020-03-18 07:22:04 +0700

    commit;

---
+++ Catalog/Implementations/ReportBE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EVE.ApiModels.Catalog;
+using EVE.Data;
+
+namespace EVE.Bussiness
+{
+    public class ReportBE :  IReportBE
+    {
+        private IUnitOfWork<EVEEntities> unitOfWork { get; set; }
+
+        public ReportBE(IUnitOfWork<EVEEntities> uoW)
+        {
+            unitOfWork = uoW;
+        }
+
+
+
+    }
+}
---
+++ Catalog/Interfaces/IReportBE.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using EVE.ApiModels.Catalog;
+using EVE.Data;
+
+namespace EVE.Bussiness
+{
+    public interface IReportBE 
+    {
+    }
+}
